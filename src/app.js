5dc533250db59c7762fa9f6c4b9cb12e8159ec26
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-05-11 19:56:46 +0700

    them env va bo sung cac package cho dev

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
-const path = require('path');
-
-const express = require('express');
-
-const errorController = require('./controllers/error');
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
+const errorController = require("./controllers/error");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use(compression());
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
-
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
