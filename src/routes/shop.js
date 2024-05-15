91aa4ec6fb77c8462a13bde4f7d949286264d8cf
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-05-15 09:42:09 +0700

    add mysql database and server.js

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -15,6 +13,8 @@ router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item',shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
